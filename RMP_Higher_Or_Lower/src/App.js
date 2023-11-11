198ae3e7236cb9ff342f95ca7d00b521ba5aa31c
Zachary Alfano <zacharyalfano2@gmail.com>
2023-11-10 23:50:25 -0500

    start react

---
+++ RMP_Higher_Or_Lower/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let x = 11;
+  x++;
+  return (
+    <div className="App">
+      <p>{x}</p>
+    </div>
+  );
+}
+
+export default App;
