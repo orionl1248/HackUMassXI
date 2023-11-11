21c649512af5d4fac78d30804a3b68d2c826ecc9
Zachary Alfano <zacharyalfano2@gmail.com>
2023-11-11 01:04:50 -0500

    added orion

--- src/GameScreen.js
+++ src/GameScreen.js
@@ -1,6 +1,7 @@
 import { testFetch } from "./scripts/rateMyProfessor"
 import { useState, useEffect } from 'react'
 import Button from '@mui/material/Button';
+import "./GameScreen.css"
 
 export default function GameScreen() {
   let [isSubmitted, setSubmitted] = useState(false);
@@ -11,13 +12,16 @@ export default function GameScreen() {
     }
   }, [isSubmitted])  
   return <>
+    <div>
+      <h1 id="gameScreenHeader">Game Screen</h1>
+    </div>
     <Button 
       onClick={()=>{setSubmitted(true)}}
       type="submit"
       fullWidth
       variant="contained"
       sx={{ mt: 3, mb: 2 }}
-    >button text</Button>
+    >Lower</Button>
     <p>Game Screen</p>
   </>
 }
